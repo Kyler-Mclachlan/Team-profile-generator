b348f778eaa6a77a49f51476c1542b29ab96806b
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-01-29 17:04:48 -0600

    working on manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,21 @@
-const Player = require('../lib/Manager.js');
+const Manager = require('../lib/Manager.js');
 
-test('creates a Manager object', () => {
-    const Manager = new Manager('Dave');
+// jest.mock('../lib/Manager.js');
+
+test('creates a Employee object', () => {
+    const manager = new Manager(25);
   
     expect(manager.name).toBe('Dave');
-    expect(manager.emailAddress).toEqual(expect.any(String));
+    // expect((employee.getName())).toEqual(expect.any(String));
+
     expect(manager.id).toEqual(expect.any(Number));
+    // expect((employee.getId())).toEqual(expect.any(Number));
+
+    expect(manager.email).toEqual(expect.any(String));
+    // expect((employee.getEmail())).toEqual(expect.any(String));
+
     expect(manager.officeNumber).toEqual(expect.any(Number));
-  
+
+    // expect(employee.role).toBe("Employee");
+    expect((manager.getRole())).toBe("Manager");
   });
